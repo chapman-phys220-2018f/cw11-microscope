83d94e9a2201d9e0551428d47ffeb3753a30453b
Enea Dodi <dodi@chapman.edu>
2018-12-08 08:16:05 +0000

    Enea's solution commit 2

--- odes.py
+++ odes.py
@@ -8,6 +8,7 @@
 # Course: PHYS220/MATH220/CPSC220 Fall 2018
 # Assignment: cw11
 ###
+
 def u(t):
     return np.array([np.cos(t),-1*np.sin(t)])
 
@@ -94,4 +95,26 @@ def secondOrderRungeKutta(N):
     a.plot(t,y,color = "red")
     plt.show()
     return 
-    
+
+def fourthOrderRungeKutta(N):
+    t = np.linspace(0,10*np.pi,N)
+    dt = t[1]-t[0]
+    x = np.zeros_like(t)
+    y = np.zeros_like(t)
+    x[0] = 1
+    y[0] = 0
+    for i in range(1,len(t)):
+        K1 = dt*derivu(t[i-1])
+        K21 = dt*derivu(t[i-1]+dt/2)
+        K22 = dt*derivu(t[i-1]+K1[1]/2)
+        K32 = dt*derivu(t[i-1]+K22[1]/2)
+        K41 = dt*derivu(t[i-1]+dt)
+        K42 = dt*derivu(t[i-1]+K32[1])
+        x[i] = x[i-1] + (K1[0]+2*K21[0]+2*K21[0]+K41[0])/6
+        y[i] = y[i-1] + (K1[1]+2*K22[1]+2*K32[1]+K42[1])/6
+    s = plt.figure(figsize=(8,6))
+    a = plt.axes()
+    a.plot(t, x, color = "blue")
+    a.plot(t,y,color = "red")
+    plt.show()
+    return 
